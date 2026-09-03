ddf1944d5f71d6ec0630e383b4d19ea3747f73fd
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-09-03 02:56:25 +0000

    bcachefs-tools: 1.39.2 -> 1.39.4
    https://github.com/koverstreet/bcachefs-tools/blob/v1.39.4/Changelog.mdwn

--- pkgs/by-name/bc/bcachefs-tools/package.nix
+++ pkgs/by-name/bc/bcachefs-tools/package.nix
@@ -31,18 +31,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bcachefs-tools";
-  version = "1.39.2";
+  version = "1.39.4";
 
   src = fetchFromGitHub {
     owner = "koverstreet";
     repo = "bcachefs-tools";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-1ilzebplgFVaqiSzpTeGRgnSqYfo9W8G7xj/6KlX990=";
+    hash = "sha256-cnSlmUH+1odSO1knyrXrWUf7EjY9U4n2IlxVKMczB3Y=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) src;
-    hash = "sha256-g3r99wa4S0ZVUvTSYo4PTzOswjsOn83hkocoziHcn2A=";
+    hash = "sha256-Jpl4SBSX9M45zFHpckNHMdZGA5y00YfhrPwmZabWj3Q=";
   };
 
   postPatch = ''
